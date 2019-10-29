943343f62b7394ed7b8460bb32d361662cf20327
michel <michel.gds@hotmail.com>
2019-10-29 11:04:59 -0300

    Configure nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'cb37022aafe54c',
+    pass: '637a8139d89aaf',
+  },
+  default: {
+    from: 'Equipe GoBraber <noreply@gobarber.com',
+  },
+};
